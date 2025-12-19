d41b8b6d40b52908de67d44109709ad8dc3b82b5
1killmonkey <danielfortnite599@gmail.com>
2025-12-18 22:01:05 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -29,14 +29,12 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/1killmonkey.github.io/', // 🔴 CLAVE
+    plugins: [react()],
 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-
-    plugins: [react()],
 
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -50,4 +48,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-
